0aedd733bfa7c87b941e0c78e05aca9a77d3b2a2
smswanlund <muelmac@gmail.com>
2019-11-12 16:05:49 -0500

    config, controllers, db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("barnyard brawl");
+INSERT INTO burgers (burger_name) VALUES ("royale with cheese");
+INSERT INTO burgers (burger_name) VALUES ("totally not a big mac");
