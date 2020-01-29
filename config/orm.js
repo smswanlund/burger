337d02aa4d1b5c83352e0328a4f3d82771eb8608
smswanlund <muelmac@gmail.com>
2020-01-29 10:56:17 -0500

    orm fix

--- config/orm.js
+++ config/orm.js
@@ -13,16 +13,10 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
     var arr = [];
 
-    for (var key in ob) {
-        var value = ob[key];
-
-        if (Object.hasOwnProperty.call(ob, key)) {
-            if (typeof value === "string" && value.indexOf(" ") > = 0) {
-                value = "'" + value + "'";
-            }
-            arr.push(key + "=" + value);
+        for (var key in ob){
+            arr.push(key + "=" + ob[key]);
         }
-    }
+     
     return arr.toString();
 }
 
